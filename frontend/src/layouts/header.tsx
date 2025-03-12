89458bdee6826fb01aaf7e4cf4fff000332416a4
Veenoway <77930262+Veenoway@users.noreply.github.com>
2025-03-12 23:29:19 +0100

    feature: add kingnad logo

--- frontend/src/layouts/header.tsx
+++ frontend/src/layouts/header.tsx
@@ -1,4 +1,5 @@
 import { WalletConnection } from "@/components/wallet-connector";
+import Image from "next/image";
 import Link from "next/link";
 import { LuExternalLink } from "react-icons/lu";
 
@@ -6,7 +7,13 @@ export const Header = () => {
   return (
     <header>
       <div className="flex justify-between items-center p-4 border-b border-white/10">
-        <p className="text-xl sm:text-2xl lg:text-3xl font-bold">KingNads</p>
+        <Image
+          src="/kingnadslogo.png"
+          alt="KingNads"
+          width={160}
+          height={120}
+          className="w-[120px] lg:w-[160px] h-auto"
+        />
         <div className="flex items-center gap-7 text-lg">
           <Link
             href="https://testnet.monadexplorer.com/token/0xd4F3ACBf68c078101E4E3a771697b2c21f5568E9"
